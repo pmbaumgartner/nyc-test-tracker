2ede53d2d1d58a3bf9d9adfa0fda5541541ea6de
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2020-12-05 13:24:55 -0500

    fix scraper to account for last u pdate values

--- scrape.py
+++ scrape.py
@@ -1,13 +1,26 @@
-import camelot
-import requests
-from tempfile import NamedTemporaryFile
-from datetime import datetime
 import hashlib
+from datetime import datetime
 from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import camelot
+import pandas as pd
+import requests
+
+
+def value_parser(cell: str):
+    values = [
+        value.replace("(cid:415)", "ti").replace("(cid:425)", "tt").replace("ﬀ", "ff")
+        for value in cell.split("\n")
+    ]
+    labels = ["Location", "Wait", "Last Updated"]
+    return dict(zip(labels, values))
+
 
 r = requests.get(
     "https://hhinternet.blob.core.windows.net/wait-times/testing-wait-times.pdf"
 )
+scrape_timestamp = datetime.now()
 
 pdf_hash = hashlib.md5(r.content).hexdigest()
 hash_location = Path("md5")
@@ -20,36 +33,18 @@ if new_file:
     hash_location.write_text(pdf_hash)
     with NamedTemporaryFile(suffix=".pdf") as temppdf:
         temppdf.write(r.content)
-        tables = camelot.read_pdf(temppdf.name, flavor="stream")
+        tables = camelot.read_pdf(
+            temppdf.name,
+            flavor="stream",
+            row_tol=30,
+            table_areas=["41,453,751,153"],
+            layout_kwargs={"detect_vertical": False},
+        )
 
-    scrape_timestamp = datetime.now()
+    all_values = tables[0].df.values.ravel()
+    parsed_values = [value_parser(cell) for cell in all_values]
+    df = pd.DataFrame(parsed_values)
 
-    df = tables[0].df
-    date = (
-        df[df[3].str.contains("|", regex=False)][3]
-        .values[0]
-        .replace(" |", ", ")
-        .replace("/", "-")
-    )
-    df_data = df[df[0].str.strip() != ""].copy()
-
-    melted_data = df_data.melt()
-    melted_data["time"] = (melted_data.index % 2).map({0: "Location", 1: "Wait"})
-    pivoted_data = melted_data[["value", "time"]].pivot(columns="time")
-    pivoted_data.columns = ["Location", "Wait"]
-    pivoted_data["Location"] = pivoted_data["Location"].fillna(method="ffill")
-    pivoted_data = pivoted_data.dropna()
-    pivoted_data["Location"] = (
-        pivoted_data["Location"]
-        .str.replace("\n", "", regex=False)
-        .str.replace("(cid:415)", "ti", regex=False)
-        .str.replace("(cid:425)", "tt", regex=False)
-    )
-    pivoted_data["Wait"] = pivoted_data["Wait"].str.replace("\n", "", regex=False)
-    pivoted_data["Time Window"] = date
-    pivoted_data["Scrape Timestamp"] = scrape_timestamp
-    pivoted_data = pivoted_data.reset_index(drop=True)
-
-    pivoted_data.to_csv(f"data/{date}-{scrape_timestamp}.csv")
-    pivoted_data.to_csv(f"data/latest.csv")
-    pivoted_data.to_csv(f"latest.csv")
+    df.to_csv(f"data/{scrape_timestamp}.csv")
+    df.to_csv("data/latest.csv")
+    df.to_csv("latest.csv")
